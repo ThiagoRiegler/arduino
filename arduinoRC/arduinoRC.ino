b84969f06306964ce194d7b81e3811a0b3ed8ca5
ThiagoRiegler <thiagoriegler123@gmail.com>
2023-05-30 09:51:38 -0300

    Update arduinoRC.ino

--- arduinoRC/arduinoRC.ino
+++ arduinoRC/arduinoRC.ino
@@ -1,12 +1,14 @@
 const int chA=A0;
 const int chB=A1;
-const int chC=A2;
-const int chD=A3;
+const int motorIzq2 = 2;
+const int motorIzq1 = 3;
+const int motorDer1 = 4;
+const int motorDer2 = 5;
+const int velIzq = 9;
+const int velDer = 10;
 
 int ch1;
 int ch2;
-int ch3;
-int ch4;
 
 void setup() {
   
@@ -14,8 +16,12 @@ void setup() {
 
   pinMode(chA, INPUT);
   pinMode(chB, INPUT);
-  pinMode(chC, INPUT);
-  pinMode(chD, INPUT);
+  pinMode(motorIzq1, OUTPUT);
+  pinMode(motorIzq2, OUTPUT);
+  pinMode(motorDer1, OUTPUT);
+  pinMode(motorDer2, OUTPUT);
+  pinMode(velIzq, OUTPUT);
+  pinMode(velDer, OUTPUT);
 
 }
 
@@ -23,17 +29,28 @@ void loop() {
   
   ch1 = pulseIn(chA, HIGH);
   ch2 = pulseIn(chB, HIGH);
-  ch3 = pulseIn(chC, HIGH);
-  ch4 = pulseIn(chD, HIGH);
   
-  if(ch2 > 1500){
-    Serial.print("adelante: ");Serial.print(map(ch2, 1500, 1990, 60, 255));Serial.print(" (");Serial.print(ch2);Serial.println(")");
+  if(ch1 > 1500){
+    digitalWrite(motorIzq1, HIGH);
+    analogWrite(velIzq, map(ch1, 1500, 1990, 60, 255));
+    digitalWrite(motorIzq2, 0);
+  }else if(ch1 < 1450){
+    digitalWrite(motorIzq2, HIGH);
+    analogWrite(velIzq, map(ch1, 990, 1450, 255, 60));
+    digitalWrite(motorIzq1, 0);
+  }else if(ch2 > 1500){
+    digitalWrite(motorDer1, HIGH);
+    analogWrite(velDer, map(ch2, 1500, 1990, 60, 255));
+    digitalWrite(motorDer2, 0);
   }else if(ch2 < 1450){
-    Serial.print("atras: ");Serial.print(map(ch2, 990, 1450, 255, 60));Serial.print(" (");Serial.print(ch2);Serial.println(")");
-  }else if(ch4 > 1500){
-    Serial.print("derecha: ");Serial.print(map(ch4, 1500, 1990, 60, 255));Serial.print(" (");Serial.print(ch4);Serial.println(")");
-  }else if(ch4 < 1450){
-    Serial.print("izquierda: ");Serial.print(map(ch4, 990, 1450, 255, 60));Serial.print(" (");Serial.print(ch4);Serial.println(")");
+    digitalWrite(motorDer2, HIGH);
+    analogWrite(velDer, map(ch2, 990, 1450, 255, 60));
+    digitalWrite(motorDer1, 0);
+  }else{
+      digitalWrite(motorDer1, 0);
+      digitalWrite(motorDer2, 0);
+      digitalWrite(motorIzq1, 0);
+      digitalWrite(motorIzq2, 0);
   }
 
 }
